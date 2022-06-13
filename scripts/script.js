95d67b436e9c6178b2fe49c34afac01385689aaa
Akoruu.Dev <akoruu.dev@gmail.com>
2022-06-13 14:20:33 -0300

    FEAT [Interação com usuário: Seleção de pedido]: Adicionado a opção de seleção de pedido, um pedido por seção, tres pedidos no total

--- scripts/script.js
+++ scripts/script.js
@@ -1,5 +1,36 @@
 function pgFinalPedido() {
-    let pgfinal = document.querySelector("#finalizacao-do-pedido");
+    let pgfinal = document.querySelector(".finalizacao-do-pedido");
+    
+    pgfinal.classList.toggle("fimpedido");
+}
 
-    pgfinal.classList.toggle("finalizacao-do-pedido");
+function test() {
+    alert("oi");
+}
+
+function selectComida(elemento) {
+    let pedidoComida = document.querySelector(".comida .pedidoSelecionado");
+    if (pedidoComida != null) {
+        pedidoComida.classList.remove("pedidoSelecionado");
+    }
+
+    elemento.classList.add("pedidoSelecionado");
+}
+
+function selectBebida(elemento) {
+    let pedidoBebida = document.querySelector(".bebida .pedidoSelecionado");
+    if (pedidoBebida != null) {
+        pedidoBebida.classList.remove("pedidoSelecionado");
+    }
+
+    elemento.classList.add("pedidoSelecionado");
+}
+
+function selectSobremesa(elemento) {
+    let pedidoSobremesa = document.querySelector(".sobremesa .pedidoSelecionado");
+    if (pedidoSobremesa != null) {
+        pedidoSobremesa.classList.remove("pedidoSelecionado");
+    }
+
+    elemento.classList.add("pedidoSelecionado");
 }

5f28b5cc8141c5fa838d6c447c6fc3a566d6fd2d
Akoruu.Dev <akoruu.dev@gmail.com>
2022-06-13 17:27:22 -0300

    FEAT [Botão de finalização de pedido: Experiência de usuário]: Mudança no pedido quando selecionar um de cada; Adicionar pagina de finalização de pedido

--- scripts/script.js
+++ scripts/script.js
@@ -8,29 +8,67 @@ function test() {
     alert("oi");
 }
 
+
+let pedidoComida;
+let valueordercom;
+let pedidoBebida;
+let valueorderbeb;
+let pedidoSobremesa;
+let valueordersob;
+
 function selectComida(elemento) {
-    let pedidoComida = document.querySelector(".comida .pedidoSelecionado");
+    pedidoComida = document.querySelector(".comida .pedidoSelecionado");
     if (pedidoComida != null) {
         pedidoComida.classList.remove("pedidoSelecionado");
     }
 
     elemento.classList.add("pedidoSelecionado");
+    pedidoComida = elemento;
+
+    valueordercom = document.querySelector(".comida .pedidoSelecionado .valueorder");
+
+    pedidoDeFinalizacao();
 }
 
 function selectBebida(elemento) {
-    let pedidoBebida = document.querySelector(".bebida .pedidoSelecionado");
+    pedidoBebida = document.querySelector(".bebida .pedidoSelecionado");
     if (pedidoBebida != null) {
         pedidoBebida.classList.remove("pedidoSelecionado");
     }
 
     elemento.classList.add("pedidoSelecionado");
+    pedidoBebida = elemento;
+
+    valueorderbeb = document.querySelector(".bebida .pedidoSelecionado .valueorder");
+
+    pedidoDeFinalizacao();
 }
 
 function selectSobremesa(elemento) {
-    let pedidoSobremesa = document.querySelector(".sobremesa .pedidoSelecionado");
+    pedidoSobremesa = document.querySelector(".sobremesa .pedidoSelecionado");
     if (pedidoSobremesa != null) {
         pedidoSobremesa.classList.remove("pedidoSelecionado");
     }
 
     elemento.classList.add("pedidoSelecionado");
+    pedidoSobremesa = elemento;
+
+    valueordersob = document.querySelector(".sobremesa .pedidoSelecionado .valueorder");
+
+    pedidoDeFinalizacao();
+}
+
+function pedidoDeFinalizacao() {
+    if (pedidoComida && pedidoBebida && pedidoSobremesa) {
+        let desaparecerbotao = document.querySelector(".selecallped");
+        desaparecerbotao.classList.add("bottonfim");
+        let botaofinalizacao = document.querySelector(".botaodepedido");
+        botaofinalizacao.classList.remove("bottonfim");
+        botaofinalizacao.classList.add("botaofinalizacao");
+
+        let comidaSelecionada = document.querySelector(".pedidocomidaped");
+        comidaSelecionada.innerHTML = pedidoComida;
+        let valuecomida = document.querySelector(".pedidocomidavalue");
+        valuecomida.innerHTML = valueordercom;
+    }   
 }
